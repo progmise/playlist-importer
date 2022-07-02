2cde5a054c425ac420bf9d2ac3d25c179bc07880
Leonel Chaves <lchaves@fi.uba.ar>
2022-07-01 23:48:37 -0300

    Se agregan funciones para validar opcion ingresada de menú

--- playlist-importer/playlist_importer.py
+++ playlist-importer/playlist_importer.py
@@ -1,5 +1,79 @@
 import csv
+
 from io import TextIOWrapper
+
+from spotify_script import agregar_canciones_a_playlist, buscar_cancion
+from spotify_script import crear_playlist, obtener_playlist, obtener_playlists
+from spotify_script import obtener_servicio, obtener_usuario_actual
+
+
+def validar_numero(numero: str) -> bool:
+
+    numero_formateado: str = str()
+    es_numero_valido: bool = bool()
+
+    numero_formateado = sub("[a-zA-Z]+", "", numero)
+
+    try:
+        int(numero_formateado)
+        es_numero_valido = True
+
+    except ValueError:
+        print("\n¡Sólo se pueden ingresar numeros!")
+        es_numero_valido = False
+
+    return es_numero_valido
+
+
+def validar_opcion(opcion: str, opciones: list) -> bool:
+
+    numero_de_opcion: int = int()
+    flag_opcion_valida: bool = False
+
+    if opcion.isnumeric():
+        numero_de_opcion = int(opcion)
+
+        if numero_de_opcion > 0 and numero_de_opcion <= len(opciones):
+            flag_opcion_valida = True
+
+        else:
+            print(f"\n¡Sólo puedes ingresar una opción entre el 1 y el {len(opciones)}!")
+
+    else:
+        print(f"\n¡Las opciones son numeros enteros, sin decimales!")
+
+    return flag_opcion_valida
+
+
+def validar_opcion_ingresada(opciones: list) -> str:
+
+    opcion_ingresada: str = str()
+    flag_numero_valido: bool = False
+    flag_opcion_valida: bool = False
+
+    while not (flag_numero_valido and flag_opcion_valida):
+
+        opcion_ingresada = input("\nIngrese una opción: ")
+
+        flag_numero_valido = validar_numero(opcion_ingresada)
+
+        flag_opcion_valida = validar_opcion(opcion_ingresada, opciones)
+
+    return opcion_ingresada
+
+
+def obtener_entrada_usuario(opciones: list) -> str:
+
+    opcion: str = str()
+
+    print("\nOpciones válidas: \n")
+
+    for x in range(len(opciones)):
+        print(x + 1, "-", opciones[x])
+
+    opcion = validar_opcion_ingresada(opciones)
+
+    return opcion
 
 
 def leer_archivo_csv(ruta_de_archivo: str) -> 'list[str]':
@@ -41,3 +115,33 @@ def escribir_archivo_csv(ruta_de_archivo: str, encabezados: list, contenido: lis
         archivo.close()     
 
 
+
+def main() -> None:
+
+    opciones: list = [
+        "Spotify",
+        "Youtube",
+        'Cerrar sesiones'
+        "Salir"
+    ]
+
+    opcion = int(obtener_entrada_usuario(opciones))
+
+    while opcion != 4:
+
+        if opcion == 1:
+            iniciar_menu_de_spotify()
+
+        elif opcion == 2:
+            pass
+
+        elif opcion == 3:
+            pass         
+
+        opcion = int(obtener_entrada_usuario(opciones))
+
+    print("\nPrograma finalizado")
+
+
+if __name__ == "__main__":
+    main()
